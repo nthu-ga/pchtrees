a3840bb32c018b630180be24bbe7d9a3f881ba6b
Andrew Cooper <apcooper@gapp.nthu.edu.tw>
2024-10-19 22:56:36 +0800

    Cleaned junk module defs from parameter_modules

--- parameter_modules.F90
+++ parameter_modules.F90
@@ -1,14 +1,3 @@
-module Stellar_Evolution
-  !
-  ! Module to hold details of stellar evolution
-  !
-  ! Integers
-  integer ntsed(2),nzmetal
-  !
-  ! Logicals
-  logical :: Output_ASCII_SSPs
-end module Stellar_Evolution
-
 module Cosmological_Parameters
   !
   ! Floats
@@ -27,18 +16,8 @@ module Run_Statistics
   data ncall /0/
   data nc1 /0/
   data nc2 /0/
-#endif 
+#endif
 end module Run_Statistics
-
-module IGM_Parameter_Data
-  ! Variables associated with the IGM
-  !
-  ! Floats
-  real VCUT,ZCUT
-  !
-  ! Logicals
-  logical absorb
-end module IGM_Parameter_Data
 
 module Halo_Mass_Function
   !
@@ -46,63 +25,3 @@ module Halo_Mass_Function
   integer massfun
   integer, parameter :: Halo_Mass_Function_PS=0,Halo_Mass_Function_SMT=1,Halo_Mass_Function_J2000=2
 end module Halo_Mass_Function
-
-module NBody_Galform
-  !
-  ! Module used to indicate to galform subroutines that this
-  ! is a run with N-body merger trees and possibly substructure,
-  ! in cases where this makes a difference.
-  !
-  ! Also used to pass N-body merger times to subroutines that need them,
-  ! in order to minimise number of routines which must be changed.
-  !
-  ! Integers
-  integer nbodyrun
-  integer subdynfric
-  integer nstepadd,nhalotree
-
-  ! Snapshot redshifts from the simulation
-  integer :: snap_first, snap_last
-  real,    allocatable, dimension(:) :: snapshot_z
-  !
-  ! Floats
-  real tcoolcut
-  real mpart, lbox
-  !
-  ! Logicals
-  logical trace_particles,use_nbody
-  !
-  ! Characters
-  character*80 parttracefile
-end module NBody_Galform
-
-
-module Milli_Tree_Arrays
-
-  use Kind_Numbers
-
-  ! Array of redshifts from the tree file
-  real, allocatable :: atrees(:)
-  ! Number of steps
-  integer, allocatable :: nhalolev(:)
-  ! Halo properties
-  integer, allocatable :: id(:),np(:),idesc(:),nsphalo(:),firstsub(:),halodecstep(:)
-  ! Subhalo properties
-  integer, allocatable :: subid(:),subnp(:),subidesc(:),subdecstep(:)
-  real, allocatable :: pos(:,:),vel(:,:),subspin(:,:)
-  real, allocatable :: veldisp(:),vmax(:)
-  integer (kind=int8byte), allocatable :: mostboundid(:)
-  real, allocatable :: rhalf(:)
-  ! First halo at each step
-  integer, allocatable :: firsthalo(:)
-  ! Mapping between input and output halo arrays
-  integer, allocatable :: mapin2out(:)
-  integer, allocatable :: mapout2in(:)
-  ! Galaxy merger times stored to save time if doing lots of outputs
-  real, allocatable :: merger_time(:)
-  integer nlev_trees_max
-end module Milli_Tree_Arrays
-
-module RCombined_Share
-  logical :: Share_Use_Stars,Share_Use_Gas
-end module RCombined_Share
