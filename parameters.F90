5bf00c07a640db016df657033028750db9a623db
Andrew Cooper <apcooper@gapp.nthu.edu.tw>
2025-03-12 14:10:39 +0800

    Added runtime and time parameters separately

--- parameters.F90
+++
@@ -1,8 +0,0 @@
-! Prompt for and read in all model parameters.
-! Many are then distributed to the subroutines that need to know them
-! via a set of modules.
-module Time_Parameters
-  ! Parameters used in making binary splits in the merger tree
-  !real eps1,eps2
-  integer istep
-end module Time_Parameters
