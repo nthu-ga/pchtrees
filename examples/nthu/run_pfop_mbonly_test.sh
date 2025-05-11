95859a4c8f80fc4a9a264a998cb1056569036a18
Andrew Cooper <apcooper@gapp.nthu.edu.tw>
2025-05-11 22:31:34 +0800

    Added test for writing main branch data only by setting pfop mass limit high

---
+++ examples/nthu/run_pfop_mbonly_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# All paths relative to executable
+./pchtrees --ntrees 20 --mphalo 1e12  --params ./pfop_mbonly_test.toml --zmax 30.0 --no-output-trees --process-first-order-progenitors
