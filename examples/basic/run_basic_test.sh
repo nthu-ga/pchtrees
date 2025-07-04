646e706786d68259ca0faf7796532f4ceafc7519
Andrew Cooper <apcooper@gapp.nthu.edu.tw>
2025-07-04 19:56:18 +0800

    Further improvements to examples

--- examples/basic/run_basic_test.sh
+++ examples/basic/run_basic_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # All paths relative to executable
-./pchtrees --ntrees 100 --mphalo 1e12  --params ./basic_test.toml --zmax 30.0
+./pchtrees --ntrees 100 --mphalo 1e12 --nlev 128 --zmax 30.0  --params ./basic_test.toml
