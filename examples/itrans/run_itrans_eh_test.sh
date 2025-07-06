1b9c8de6e07be362b0a37bb54dd23bb2ba755a1e
Andrew Cooper <apcooper@gapp.nthu.edu.tw>
2025-07-06 23:07:43 +0800

    Adds itrans example dir

---
+++ examples/itrans/run_itrans_eh_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# All paths relative to executable
+./pchtrees --ntrees 100 --mphalo 1e12 --nlev 128 --zmax 30.0  --params ./itrans_eh_test.toml
---
+++ examples/itrans/run_itrans_tabulated_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# All paths relative to executable
+./pchtrees --ntrees 1000 --mphalo 1e12 --nlev 128 --zmax 30.0  --params ./itrans_tabulated_test.toml
